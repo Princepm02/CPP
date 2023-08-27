a4f392687401935972fddfc3accc1ffce7737ec2
Prince Mishra <princemishraa588@gmail.com>
2023-08-27 17:04:00 +0530

    Create Fibonacci.cpp

---
+++ Basics/Fibonacci.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a = 0;
+    int b = 1;
+    int c;
+    
+    cout << "Enter the number of elements you want in Fibonacci series: ";
+    int n;
+    cin >> n;
+    
+    cout << "Fibonacci series: " << a << " " << b;
+    
+    for (int i = 0; i < n - 2; i++) 
+    {
+        c = a + b;
+        cout << " " << c;
+        a = b;
+        b = c;
+    }
+    
+    return 0;
+}
