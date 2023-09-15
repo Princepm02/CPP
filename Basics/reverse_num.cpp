9f7d0cf645e5e5c3b47b2914b3873b471a453dd1
Prince Mishra <princemishraa588@gmail.com>
2023-09-15 19:53:45 +0530

    Create reverse_num.cpp

---
+++ Basics/reverse_num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int num, rev = 0, rem;
+
+  cout << "Enter an integer : ";
+  cin >> num;
+
+  while(num != 0) {
+    rem = num % 10;
+    rev = (rev * 10) + rem;
+    num = num / 10;
+  }
+
+  cout << "Reverse of Given Number = " << rev;
+
+  return 0;
+}
